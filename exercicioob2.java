3d71e44f6f43c0eff55786b96e028b2973426442
Diogo Pincetta <diogohe08@gmail.com>
2024-02-09 13:59:19 -0300

    Exercicio 2

---
+++ exercicioob2.java
@@ -0,0 +1,34 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Employee empl = new Employee();
+		
+		System.out.println("Name:");
+		empl.name = sc.nextLine();
+		System.out.println("Gross Salary:");
+		empl.grossSalary = sc.nextDouble();
+		System.out.println("Tax:");
+		empl.tax = sc.nextDouble();
+		
+		System.out.println("Employee :" + empl);
+		
+		System.out.println("Wich percentage to increase salary");
+		double percentage =  sc.nextDouble();
+		empl.increaseSalary(percentage);
+		
+		System.out.println("Update data :" + empl);
+		
+		
+		sc.close();
+	}
+
+}
